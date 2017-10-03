b7092838edf8af6effc508f083cbcaecbd2b0ab2
dat13njo <dat13njo@student.lu.se>
2017-10-03 10:02:27 +0200

    clear

--- Lab5/timebase.h
+++
@@ -1,9 +0,0 @@
-extern "C" {
-
-void			init_timebase(void);
-unsigned long long	tbr(void);
-unsigned long long	timebase(void);
-double			timebase_sec(void);
-double			timebase_cycles(unsigned long long timebase_count);
-
-}
