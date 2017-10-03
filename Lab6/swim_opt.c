3d34d020d0c8f3005959109350e172988de25814
dat13njo <dat13njo@student.lu.se>
2017-10-03 11:36:33 +0200

    power

--- Lab6/swim_opt.c
+++ Lab6/swim_opt.c
@@ -223,24 +223,47 @@ L90:
     ucheck = 0.;
     vcheck = 0.;
     i__1 = mnmin;
+    // *********************************************
     #pragma omp parallel for
     #pragma reduction(+:pcheck)
-    #pragma reduction(+:ucheck)
-    #pragma reduction(+:vcheck)
     for (icheck = 1; icheck <= i__1; ++icheck) {
 	i__2 = mnmin;
 	for (jcheck = 1; jcheck <= i__2; ++jcheck) {
 	    pcheck += (d__1 = _BLNK__1.pnew[icheck + jcheck * 1335 - 1336],
-		    abs(d__1));
-	    ucheck += (d__1 = _BLNK__1.unew[icheck + jcheck * 1335 - 1336],
-		    abs(d__1));
-	    vcheck += (d__1 = _BLNK__1.vnew[icheck + jcheck * 1335 - 1336],
 		    abs(d__1));
 /* L4500: */
 	}
 	_BLNK__1.unew[icheck + icheck * 1335 - 1336] *= icheck % 100 / 100.f;
 /* L3500: */
     }
+
+    #pragma omp parallel for
+    #pragma reduction(+:ucheck)
+    for (icheck = 1; icheck <= i__1; ++icheck) {
+  i__2 = mnmin;
+  for (jcheck = 1; jcheck <= i__2; ++jcheck) {
+      ucheck += (d__1 = _BLNK__1.unew[icheck + jcheck * 1335 - 1336],
+        abs(d__1));
+/* L4500: */
+  }
+/* L3500: */
+    }
+
+
+    #pragma omp parallel for
+    #pragma reduction(+:vcheck)
+    for (icheck = 1; icheck <= i__1; ++icheck) {
+	i__2 = mnmin;
+	for (jcheck = 1; jcheck <= i__2; ++jcheck) {
+	    vcheck += (d__1 = _BLNK__1.vnew[icheck + jcheck * 1335 - 1336],
+		    abs(d__1));
+/* L4500: */
+	}
+/* L3500: */
+    }
+
+
+    // *********************************************
 /* *** */
 /* *** */
     s_wsfe(&io___16);
